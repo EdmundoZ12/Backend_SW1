d629ccea182d825d413e715a4ff52651701e359e
CesarKnight <cesarcaballero111@gmail.com>
2024-11-26 22:49:36 -0400

    aumentar tamaño de peticion http para poder subir imagenes hasta de 50Mb

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ limit: '50mb', extended: true }));
 
   app.enableCors();
   const config = new DocumentBuilder()
